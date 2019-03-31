6e4434de9d11d7217c7316ea75b8dcc5ecdd2f18
Stephen Flynn <stephen.flynn@jftechnology.com>
2019-03-31 14:29:33 +0100

    fix: travis config

--- config/ember-try.js
+++ config/ember-try.js
@@ -10,23 +10,11 @@ module.exports = function() {
   ]).then((urls) => {
     return {
       scenarios: [
-        {
-          name: 'ember-lts-2.18',
-          env: {
-            EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true })
-          },
+         {
+          name: 'ember-lts-3.8',
           npm: {
             devDependencies: {
-              '@ember/jquery': '^0.5.1',
-              'ember-source': '~2.18.0'
-            }
-          }
-        },
-        {
-          name: 'ember-lts-3.4',
-          npm: {
-            devDependencies: {
-              'ember-source': '~3.4.0'
+              'ember-source': '~3.8.0'
             }
           }
         },
