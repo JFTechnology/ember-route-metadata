f9045e7ff67808e12a39b02cb2d7b83be85a878d
Stephen Flynn <stephen.flynn@jftechnology.com>
2019-12-17 20:50:39 +0000

    chore: ember upgrade 3.14

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,11 @@ module.exports = {
   root: true,
   parser: 'babel-eslint',
   parserOptions: {
-    ecmaVersion: 2017,
-    sourceType: 'module'
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      legacyDecorators: true
+    }
   },
   plugins: [
     'ember'
@@ -20,6 +23,7 @@ module.exports = {
   },
   rules: {
     "no-console": "warn",
+    'ember/no-jquery': 'error'
   },
   overrides: [
     // node files
@@ -41,8 +45,7 @@ module.exports = {
         'tests/dummy/app/**'
       ],
       parserOptions: {
-        sourceType: 'script',
-        ecmaVersion: 2015
+        sourceType: 'script'
       },
       env: {
         browser: false,
